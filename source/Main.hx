0e10e992bed89cc84a518d6609ad5c6ecb2c95fd
Logan <monday.hopscotch@gmail.com>
2020-12-05 20:48:57 -0700

    move sound and mouse changes to AFTER the FlxGame is created

--- source/Main.hx
+++ source/Main.hx
@@ -17,14 +17,6 @@ class Main extends Sprite
 		Configure.initAnalytics();
 		FlxG.fixedTimestep = false;
 
-		// Disable flixel volume controls as we don't use them because of FMOD
-		FlxG.sound.muteKeys = null;
-		FlxG.sound.volumeUpKeys = null;
-		FlxG.sound.volumeDownKeys = null;
-
-		// Don't use the flixel cursor
-		FlxG.mouse.useSystemCursor = true;
-
 		#if debug
 		FlxG.autoPause = false;
 		#end
@@ -32,5 +24,13 @@ class Main extends Sprite
 		FlxTransitionableState.defaultTransIn = new TransitionData(FADE, FlxColor.BLACK, 0.35);
 		FlxTransitionableState.defaultTransOut = new TransitionData(FADE, FlxColor.BLACK, 0.35);
 		addChild(new FlxGame(0, 0, LoadFmodState, 1, 60, 60, true, false));
+
+		// Disable flixel volume controls as we don't use them because of FMOD
+		FlxG.sound.muteKeys = null;
+		FlxG.sound.volumeUpKeys = null;
+		FlxG.sound.volumeDownKeys = null;
+
+		// Don't use the flixel cursor
+		FlxG.mouse.useSystemCursor = true;
 	}
 }
