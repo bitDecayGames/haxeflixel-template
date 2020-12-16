465670759d6f4225e18be550ca08b4ee253a0e85
Logan <monday.hopscotch@gmail.com>
2020-12-16 00:13:14 -0700

    update init.sh to provide user with commands to restore haxelib dev libs

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+restoreDevCommands=()
+
 while read line; do
   echo ""
 
@@ -15,9 +17,59 @@ while read line; do
 
   libName="${splits[0]}"
   libVersion="${splits[1]}"
-  
-  haxelib dev ${libName}
 
+  libInfo="$(haxelib list ${libName})"
+
+  # Replace all backslashes with forward slashes for bash friendliness
+  # when working with windows paths
+  libInfo="${libInfo//\\//}"
+
+  if [[ ${libInfo} == *"dev"* ]]; then
+    ###############################################################
+    # We found a dev dependency. We want to disable this because  #
+    # when dev is enabled, it seems to override any other version #
+    # that is set in haxelib. However, we also want to give the   #
+    # user the command to restore them as finding the correct     #
+    # path again is potentially annoying.                         #
+    ###############################################################
+
+    # Disable the dev lib
+    haxelib dev ${libName}
+
+    infSplits=(${libInfo})
+
+    # Save the file separator so we can later restore it
+    SAVE_IFS=$IFS
+    for i in "${infSplits[@]}"
+    do
+      if [[ ${i} == *"dev"* ]]; then
+        # remove first and last characters which are [ and ]
+        devVersionSegment="${i:1:${#i}-2}"
+
+        # Set comma as delimiter to handle windows paths
+        IFS=':'
+
+        # Read the split words into an array
+        read -a devArr <<< "${devVersionSegment}"
+
+        # remove the first element which will be 'dev'
+        unset 'devArr[0]'
+
+        # Turn array into space-delimited string
+        devPath="${devArr[@]}"
+
+        # Replace spaces with ':' to make it back into a path
+        devPathJoined=${devPath// /:}
+
+        # Save the command to restore the depenency
+        restoreDevCommands+=("haxelib dev ${libName} ${devPathJoined}")
+      fi
+    done
+    # restore the file separator
+    IFS=$SAVE_IFS
+  fi
+
+  # Now we can handle actually installing the needed version
   if [[ ${libVersion} == "git" ]]; then
     gitLocation="${splits[2]}"
     gitBranchOrTag="${splits[3]}"
@@ -33,3 +85,14 @@ while read line; do
     haxelib set ${libName} ${libVersion} --always
   fi
 done <haxelib.deps
+
+if [ ${#restoreDevCommands[@]} -ne 0 ]; then
+    echo ""
+    echo "Some dev dependencies were disabled."
+    echo "To restore them, run:"
+
+    for value in "${restoreDevCommands[@]}"
+    do
+        echo "${value}"
+    done
+fi
