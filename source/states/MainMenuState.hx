fd6dd1864950191244b2db0927b774926ba712ab
Logan <monday.hopscotch@gmail.com>
2021-02-07 22:51:32 -0700

    first pass of getting controller/cursor support into main menu

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -1,5 +1,10 @@
 package states;
 
+import flixel.addons.ui.FlxUITypedButton;
+import flixel.addons.ui.FlxUIButton;
+import flixel.addons.ui.FlxUICheckBox;
+import flixel.addons.ui.FlxUI;
+import flixel.addons.ui.FlxUICursor;
 import haxefmod.flixel.FmodFlxUtilities;
 import flixel.text.FlxText;
 import flixel.FlxG;
@@ -22,45 +27,70 @@ class MainMenuState extends FlxUIState {
 	var _txtTitle:FlxText;
 
 	override public function create():Void {
+		_xml_id = "main_menu";
+		_makeCursor = true;
 		super.create();
+
+		cursor.loadGraphic(AssetPaths.pointer__png, true, 32, 32);
+		cursor.animation.add("pointing", [0, 1], 3);
+		cursor.animation.play("pointing");
+
+		// Allow menu control with various control methods
+		cursor.setDefaultKeys(FlxUICursor.KEYS_ARROWS | FlxUICursor.KEYS_WASD | FlxUICursor.GAMEPAD_DPAD);
+
 		FmodManager.PlaySong(FmodSongs.LetsGo);
 		FlxG.log.notice("loaded scene");
 		bgColor = FlxColor.TRANSPARENT;
 		FlxG.camera.pixelPerfectRender = true;
 
-		_txtTitle = FlxTextFactory.make("Game Title", FlxG.width / 2, FlxG.height / 4, 40, FlxTextAlign.CENTER, FlxColor.WHITE);
-		add(_txtTitle);
+		// TODO: Still need to rig up the actual behavior here
 
-		_btnPlay = UiHelpers.createMenuButton("Play", clickPlay);
-		_btnPlay.setPosition(FlxG.width / 2 - _btnPlay.width / 2, FlxG.height - _btnPlay.height - 100);
-		_btnPlay.updateHitbox();
-		add(_btnPlay);
+		// _btnPlay = UiHelpers.createMenuButton("Play", clickPlay);
+		// _btnPlay.setPosition(FlxG.width / 2 - _btnPlay.width / 2, FlxG.height - _btnPlay.height - 100);
+		// _btnPlay.updateHitbox();
+		// add(_btnPlay);
 
-		_btnCredits = UiHelpers.createMenuButton("Credits", clickCredits);
-		_btnCredits.setPosition(FlxG.width / 2 - _btnCredits.width / 2, FlxG.height - _btnCredits.height - 70);
-		_btnCredits.updateHitbox();
-		add(_btnCredits);
+		// _btnCredits = UiHelpers.createMenuButton("Credits", clickCredits);
+		// _btnCredits.setPosition(FlxG.width / 2 - _btnCredits.width / 2, FlxG.height - _btnCredits.height - 70);
+		// _btnCredits.updateHitbox();
+		// add(_btnCredits);
 
-		#if windows
-		_btnExit = UiHelpers.createMenuButton("Exit", clickExit);
-		_btnExit.setPosition(FlxG.width / 2 - _btnExit.width / 2, FlxG.height - _btnExit.height - 40);
-		_btnExit.updateHitbox();
-		add(_btnExit);
-		#end
+		// #if windows
+		// _btnExit = UiHelpers.createMenuButton("Exit", clickExit);
+		// _btnExit.setPosition(FlxG.width / 2 - _btnExit.width / 2, FlxG.height - _btnExit.height - 40);
+		// _btnExit.updateHitbox();
+		// add(_btnExit);
+		// #end
+	}
+
+	override public function getEvent(name:String, sender:Dynamic, data:Dynamic, ?params:Array<Dynamic>):Void {
+		if (name == FlxUITypedButton.CLICK_EVENT) {
+			trace("click!");
+			var button_action:String = params[0];
+			trace('Action: "${button_action}"');
+
+			if (button_action == "play") {
+				clickPlay();
+			}
+
+			if (button_action == "credits") {
+				clickCredits();
+			}
+		}
 	}
 
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
 		FmodManager.Update();
-
-		_txtTitle.x = FlxG.width / 2 - _txtTitle.width / 2;
 	}
 
 	function clickPlay():Void {
+		trace("playing");
 		FmodFlxUtilities.TransitionToStateAndStopMusic(new VictoryState());
 	}
 
 	function clickCredits():Void {
+		trace("crediting");
 		FmodFlxUtilities.TransitionToState(new CreditsState());
 	}
 
