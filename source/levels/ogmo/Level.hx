a8f5ac59a330528cb70555b50c3be7ad67322b54
Logan <monday.hopscotch@gmail.com>
2021-03-19 20:29:40 -0600

    more cleanup of level parsing

--- source/levels/ogmo/Level.hx
+++ source/levels/ogmo/Level.hx
@@ -2,23 +2,18 @@ package levels.ogmo;
 
 import flixel.FlxBasic;
 import flixel.FlxObject;
-import flixel.FlxSprite;
 import flixel.addons.editors.ogmo.FlxOgmo3Loader;
-import flixel.addons.nape.FlxNapeTilemap;
 import flixel.group.FlxGroup;
-import flixel.math.FlxVector;
 import flixel.tile.FlxTilemap;
 
 class Level {
-	public var player:Spaceman;
-
 	public var layer:FlxTilemap;
 
 	public function new(level:String) {
-		var loader = new FlxOgmo3Loader(AssetPaths.levels__ogmo, level);
+		var loader = new FlxOgmo3Loader("<AssetPath to ogmo file>", level);
 		layer = loader.loadTilemap("<AssetPath to tilemap for layer>", "<layer name>");
 
-		objects = new FlxGroup();
+		var objects = new FlxGroup();
 
 		loader.loadEntities((entityData) -> {
 			var obj:FlxBasic;
