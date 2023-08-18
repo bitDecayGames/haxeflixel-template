6a909ef2385e9569a2bb74e9aada0c7e16bf2691
Logan <monday.hopscotch@gmail.com>
2023-08-17 22:35:45 -0600

    ldtk stub for ease of remembering how to use it

---
+++ source/levels/ldtk/LDTKProject.hx
@@ -0,0 +1,5 @@
+package levels.ldtk;
+
+// Uncomment and point this at your project file for the macro magic to do its thing
+// private typedef _Tmp =
+//     haxe.macro.MacroType<[ ldtk.Project.build("assets/world.ldtk") ]>;
