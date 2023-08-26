1d6774c1996d6cf9a4f9b8ba6dbc12930bec0472
Logan <monday.hopscotch@gmail.com>
2023-08-26 11:47:43 -0600

    metrics visualization via grafana container and docker-compose

---
+++ bin/view_metrics.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if ! command -v docker &> /dev/null
+then
+	echo -e "${RED}ERROR: docker command could not be found. Please install docker"
+	exit 1
+fi
+
+if ! command -v docker-compose &> /dev/null
+then
+	echo -e "${RED}ERROR: docker-compose command could not be found. Please install docker-compose"
+	exit 1
+fi
+
+if [ ! -f "./metrics/.env" ]; then
+	echo "no env file found. Generating:"
+	read -p "Enter InfluxDB read token: " influxToken
+	echo INFLUX_TOKEN=${influxToken} > ./metrics/.env
+fi
+
+if [ ! -d "./metrics/data" ]; then
+	echo "creating data dir for grafana container"
+	mkdir ./metrics/data/
+fi
+
+docker-compose -f ./metrics/docker-compose.yml up
