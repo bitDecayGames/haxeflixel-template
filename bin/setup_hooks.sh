73efcdd876c8db32ca5ac648b5870a9bfdc93f08
Logan <monday.hopscotch@gmail.com>
2023-10-05 21:30:34 -0600

    provide some information when running setup_hooks

--- bin/setup_hooks.sh
+++ bin/setup_hooks.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+set -e
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "Copying hooks into .git directory"
 cp -a $SCRIPT_DIR/hooks/. $SCRIPT_DIR/../.git/hooks/
+echo "Done"
