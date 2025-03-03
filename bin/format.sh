0eb6369754fe5c0530d00426d41bd9eb81bdf41c
Logan <monday.hopscotch@gmail.com>
2025-03-03 13:43:43 -0700

    update format script so hook behaves as intended

--- bin/format.sh
+++ bin/format.sh
@@ -1,2 +1,19 @@
 haxelib install formatter
-haxelib run formatter -s source/
+
+# Check if any arguments were passed
+if [ $# -eq 0 ]; then
+  echo "No files passed in, formatting /source directory"
+  haxelib run formatter -s source/
+  exit 0
+fi
+
+ARGS=()
+for file in $@; do
+  # echo "Staged file: $file"
+  if [[ -f "$file" ]]; then
+    # echo "Adding arg file: $file"
+    ARGS+=("-s" "$file")  # Each file gets prefixed with -s
+  fi
+done
+echo "Checking formatting on $# .hx files..."
+haxelib run formatter "${ARGS[@]}"
